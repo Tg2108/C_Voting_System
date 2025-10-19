1e9e65ba4430e3be3c83c0d31da3c91d1fd49a1b
Tg2108 <geethchamikara0000@gmail.com>
2025-10-19 17:47:59 +0530

    Creat addmin part

--- c_files/MainFunction.c
+++ c_files/MainFunction.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-FILE *fp1;
+FILE *ac;
+FILE *nc;
+FILE *file;
 char name_from_file[50], avg_from_file[10];
 char name_from_user[50], avg_from_user[10];
-int found = 0,x;
-int ex1(){
+char ch;
+char line[256];
+int found = 0,x,c;
+int main(){
 
 printf("\t\t\t\t\t1.Vorting\n");
 printf("\t\t\t\t\t2.Result\n");
@@ -20,14 +24,14 @@ switch(x){
     printf("Enter Your Passward: ");
     scanf("%s", avg_from_user);
 
-    fp1 = fopen("my.txt", "r");
-    if (fp1 == NULL) {
+    file = fopen("my.txt", "r");
+    if (file == NULL) {
         printf("Error opening file!\n");
         return 1; 
     }
 
     
-    while(fscanf(fp1, "%s%s", name_from_file, avg_from_file) == 2){
+    while(fscanf(file, "%s%s", name_from_file, avg_from_file) == 2){
 
         if(strcmp(name_from_file, name_from_user) == 0 && strcmp(avg_from_file, avg_from_user) == 0){
             printf("\n\n\tYou Successful Loging..\n\n");
@@ -52,14 +56,14 @@ switch(x){
     printf("Enter Your Passward: ");
     scanf("%s", avg_from_user);
 
-    fp1 = fopen("my.txt", "r");
-    if (fp1 == NULL) {
+    file = fopen("my.txt", "r");
+    if (file == NULL) {
         printf("Error opening file!\n");
         return 1; 
     }
 
     
-    while(fscanf(fp1, "%s%s", name_from_file, avg_from_file) == 2){
+    while(fscanf(file, "%s%s", name_from_file, avg_from_file) == 2){
         
         if(strcmp(name_from_file, name_from_user) == 0 && strcmp(avg_from_file, avg_from_user) == 0){
             printf("\n\n\tYou Successful Loging..\n\n");
@@ -77,22 +81,118 @@ switch(x){
         break;
 
     case 3:
-    printf("Enter Your Username (Your NIC): ");
-    scanf("%s", name_from_user);
 
-    printf("Enter Your Passward: ");
-    scanf("%s", avg_from_user);
+            printf("Enter Your Username (Your NIC): ");
+            scanf("%s", name_from_user);
 
-    
+            printf("Enter Your Passward: ");
+            scanf("%s", avg_from_user);
+            
+                    printf("\n\t\t\t\t\t1.CANDIDATES\n");
+                    printf("\t\t\t\t\t2.VOTERS\n");
+                    printf("\t\t\t\t\t3.POLLITICAL PARTYS\n\n");
+
+            printf("\t\t\t\tENTER YOUR CHOISE :");
+            scanf("%d",&c);
+
+    switch(c){
+
+        case 1:
+                file = fopen("my.txt", "r");//candidate details file
+                if (file == NULL) {
+                    printf("Error opening file!\n");
+                    return 1;
+                }
+
+                printf("\tAre you want Aprove Press '1'\n \tAre you want NotAprove Press '0'\n\n");
+
+
+                while (fgets(line, sizeof(line), file)) {
+                    printf("%s", line);
+                    printf("Aprove or Not Aprove: ");
+                    ch = getchar();
+                    if(ch == '1') {
+                        ac= fopen("approv","a");
+                        fprintf(ac,"%s\n",line);
+                    }else if (ch == '0'){
+                            nc = fopen("notapprov","a");
+                            fprintf(nc,"%s\n",line);
+                        }
+                    
+                    while ((ch = getchar()) != '\n' && ch != EOF);
+                }
+
+                fclose(file);
+                break;
+
+        case 2:
 
 
 
-    
+                file = fopen("my.txt", "r");//voters details file
+                if (file == NULL) {
+                    printf("Error opening file!\n");
+                    return 1;
+                }
+
+                printf("\tAre you want Aprove Press '1'\n \tAre you want NotAprove Press '0'\n\n");
+
+
+                while (fgets(line, sizeof(line), file)) {
+                    printf("%s", line);
+                    printf("Aprove or Not Aprove: ");
+                    ch = getchar();
+                    if(ch == '1') {
+                        ac= fopen("approv","a");
+                        fprintf(ac,"%s\n",line);
+                    }else if (ch == '0'){
+                            nc = fopen("notapprov","a");
+                            fprintf(nc,"%s\n",line);
+                        }
+                    
+                    while ((ch = getchar()) != '\n' && ch != EOF);
+                }
+
+                fclose(file);
+                break;
+
         
-        break;
+        case 3:
 
+
+
+                file = fopen("my.txt", "r");//party file
+                if (file == NULL) {
+                    printf("Error opening file!\n");
+                    return 1;
+                }
+
+                printf("\tAre you want Aprove Press '1'\n \tAre you want NotAprove Press '0'\n\n");
+
+
+                while (fgets(line, sizeof(line), file)) {
+                    printf("%s", line);
+                    printf("Aprove or Not Aprove: ");
+                    ch = getchar();
+                    if(ch == '1') {
+                        ac= fopen("approv","a");
+                        fprintf(ac,"%s\n",line);
+                    }else if (ch == '0'){
+                            nc = fopen("notapprov","a");
+                            fprintf(nc,"%s\n",line);
+                        }
+                    
+                    while ((ch = getchar()) != '\n' && ch != EOF);
+                }
+
+                fclose(file);
+                break;
+
+    }
+}
+    
 }
 
 
-}
+
 

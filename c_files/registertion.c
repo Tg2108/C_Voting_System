fbe1bfbe39b5ddfcbe37709553d6a0ce88eba2ba
TG20242113 <kavishanpasindu2004@gmail.com>
2025-10-23 22:14:09 +0530

    newupdate

--- c_files/registertion.c
+++ c_files/registertion.c
@@ -2,12 +2,11 @@
 #include<math.h>
 #include<string.h>
 #include<stdlib.h>
-
 int main(){  
     system("cls");
     char W[100],R[10],T[100],O[100],E[12],L[50],K[12],J[8],H[100],F[10],D[100];
     char A[25],Z[100],X[12],C[8],V[100],B[50],N[12],M[50],G[12],Y[100],P[12],PA[12];
-    int U,Q,S,I,rl; 
+    int U,Q,S,rl,I; 
     
     printf("\n\n\n\n\n\n\n\n\t\t\t\t\t---- WELLCOME TO PARLIAMENT ELECTION VOTING SYSTEM 2025 ----\n");
     printf("\t\t\t\t\t...............................................................\n\n");
@@ -24,7 +23,6 @@ int main(){
     FILE *candi1;
     FILE *candi2;
     FILE *vot1;
-    FILE *vot2;
     FILE *party1;
     FILE *party2;
 
@@ -46,11 +44,6 @@ int main(){
             return 1;
         }
 
-    vot2=fopen("V_hide.txt","a");
-        if (vot2== NULL) {
-            printf("Error opening file for writing!\n");
-            return 1;
-        }
     party1=fopen("P_Details.txt","a");
         if (party1== NULL) {
             printf("Error opening file for writing!\n");
@@ -89,27 +82,20 @@ int main(){
             if (size1 == 0) {
                 fprintf(candi2, "ID_NUM,USER_NAME,PASSWORD\n");
             }
+
             fseek(vot1, 0, SEEK_END);
             long size2 = ftell(vot1);
             if (size2 == 0) {
                 fprintf(vot1, "NAME,ID_NUM,DOB,G-mail,PARTY_NAME,ZIP_CODE,DISTRICT,ADDRESS\n");
             }
-            fseek(vot2, 0, SEEK_END);
-            long size3 = ftell(vot2);
-            if (size3 == 0) {
-                fprintf(vot2, "ID_NUM,USER_NAME,PASSWORD\n");
-            }
+
             fseek(party1, 0, SEEK_END);
             long size4 = ftell(party1);
             if (size4 == 0) {
                 fprintf(party1, "PARTY_NAME,Chirmen_NAME,Chirmen_ID,Secratery_NAME,Secratery_ID\n");
             }
-            fseek(party2, 0, SEEK_END);
-            long size5 = ftell(party2);
-            if (size5 == 0) {
-                fprintf(party2, "PARTY_NAME,PASSWORD\n");
-            }
-            
+
+
         switch(Q){
                  
             case 1:
@@ -118,6 +104,28 @@ int main(){
                 printf("\t\t\t\t...............................................................\n\n");
                 printf("\n\t\t\t\t||\t#### ENTER YOUR CANDIDATES DATAILS HERE ####         ||\n\n");
                 printf("\t\t\t\t...............................................................\n\n");
+
+                FILE *file;
+                char line[100];
+                char Word[100];
+                int Number = 0;
+
+                file = fopen("P_Details.txt", "r");
+                if (file == NULL) {
+                    printf("Error opening file!\n");
+                    
+                }
+
+                while (fgets(line, sizeof(line), file) != NULL) {
+                    Number++;
+                    if (sscanf(line, "%99s", Word) == 1) {
+                        printf("%d.%s\n",Number,Word);
+                    } else {
+                        printf("Line %d is empty or has no word.\n", Number);
+                    }
+                }
+
+                fclose(file);
 
                 printf("\t\tENTER YOUR FULL NAME             :");
                 scanf("%s",W);
@@ -155,130 +163,13 @@ int main(){
                 scanf("%d",&U);
                 fprintf(candi1,"\t%d",U);
 
-                printf("\n\t\tSELECT YOUR DISTRICT NUMBER:\n");
-                printf("\t\t1.Ampara         2.Anuradhapura   3.Badulla        4.Batticaloa     5.Colombo\n\t\t6.Galle          7.Gampaha        8.Hambantota     ");
-                printf("9.Jaffna         10.Kalutara\n\t\t11.Kandy         12.Kegalle       13.Kilinochchi   14.Kurunagala    15.Mannarama\n\t\t16.Matale        ");
-                printf("17.Matara        18.Monaragala    19.Mullaitivu    20.Nuwara Eliya  \n\t\t21.Polonnaruwa   22.Puttalam      23.Ratnapura     24.Trincomalee   25.Vavuniya\n");
-                scanf("\t\t\t:::%d",&I);
-                
-                if(I>=1 && I<=25){
-                    switch(I){
-                        case 1:
-                            printf("Ampara");
-                            fprintf(candi1,"\t%d.Ampara",I);
-                            break;
-                        case 2:
-                            printf("Anuradhapura");
-                            fprintf(candi1,"\t%d.Anuradhapura",I);
-                            break;
-                        case 3:
-                            printf("Badulla");
-                            fprintf(candi1,"\t%d.Badulla",I); 
-                            break;
-                        case 4:
-                            printf("Batticaloa");
-                            fprintf(candi1,"\t%d.Batticaloa",I);
-                            break;
-                        case 5:
-                            printf("Colombo");
-                            fprintf(candi1,"\t%d.Colombo",I);
-                            break;
-                        case 6:
-                            printf("Galle");
-                            fprintf(candi1,"\t%d.Galle",I);
-                            break;
-                        case 7:
-                            printf("Gampaha");
-                            fprintf(candi1,"\t%d.Gampaha",I);
-                            break;
-                        case 8:
-                            printf("Hambantota");
-                            fprintf(candi1,"\t%d.Hambantota",I);
-                            break;
-                        case 9:
-                            printf("Jaffna");
-                            fprintf(candi1,"\t%d.Jaffna",I);
-                            break;
-                        case 10:
-                            printf("Kalutara");
-                            fprintf(candi1,"\t%d.kalutara",I);
-                            break;
-                        case 11:
-                            printf("Kandy");
-                            fprintf(candi1,"\t%d.Kandy",I);
-                            break;
-                        case 12:
-                            printf("Kegalle");
-                            fprintf(candi1,"\t%d.Kegalle",I);
-                            break;
-                        case 13:
-                            printf("Kilinochchi");
-                            fprintf(candi1,"\t%d.Kilinochchi",I);
-                            break;
-                        case 14:
-                            printf("Kurunagala");
-                            fprintf(candi1,"\t%d.Kurunagala",I);
-                            break;
-                        case 15:
-                            printf("Mannarama");
-                            fprintf(candi1,"\t%d.Mannarama",I);
-                            break;
-                        case 16:
-                            printf("Matale");
-                            fprintf(candi1,"\t%d.Matale",I);
-                            break;
-                        case 17:
-                            printf("Matara");
-                            fprintf(candi1,"\t%d.Matara",I);
-                            break;
-                        case 18:
-                            printf("Monaragala");
-                            fprintf(candi1,"\t%d.Monaragala",I);
-                            break;
-                        case 19:
-                            printf("Mullaitivu");
-                            fprintf(candi1,"\t%d.Mullaitive",I);
-                            break;
-                        case 20:
-                            printf("Nuwara Eliya");
-                            fprintf(candi1,"\t%d.Nuwara Eliya",I);
-                            break;
-                        case 21:
-                            printf("Polonnaruwa");
-                            fprintf(candi1,"\t%d.Polonnaruwa",I);
-                            break;
-                        case 22:
-                            printf("Puttalam");
-                            fprintf(candi1,"\t%d.Puttalam",I);
-                            break;
-                        case 23:
-                            printf("Rathnapura");
-                            fprintf(candi1,"\t%d.Rathnapura",I);
-                            break;
-                        case 24:
-                            printf("Trincomalee");
-                            fprintf(candi1,"\t%d.Trincomalee",I);
-                            break;
-                        case 25:
-                            printf("Vavuniya");
-                            fprintf(candi1,"\t%d.Vavuniya",I);
-                            break;
-                        default:
-                            printf("This is invalid number:");
-                            break;
-                        }     
-                }else{
-                    printf("This number must be between 1-25");   
-                } 
-                fprintf(candi1,"\t%s",I);
-
                 printf("\n\t\tYOUR ADDRESS       :");
                 scanf("%s",O);
                 fprintf(candi1,"\t%s",O);
                 
                 printf("\n\t\tUSERNAME  :");
                 scanf("%s",K);
-                fprintf(candi2,"\t%s",K);
+                fprintf(candi2,"\n%s",K);
 
                 printf("\n\t\t\t* include 6 Characters and Don't include a simbls.");
                 printf("\n\t\tPASSWORD  :");
@@ -316,7 +207,6 @@ int main(){
                 }
                 printf("\n\t\tYour ID number is accepted: %s\n", G);
                 fprintf(vot1,"\t%s",G);
-                fprintf(vot2,"\n%s",G);
 
                 printf("\n\t\tYOUR DATE OF BIRTH(YYYY.MM.DD) :");
                 scanf("%s",F);
@@ -326,25 +216,17 @@ int main(){
                 scanf("%s",D);
                 fprintf(vot1,"\t%s",D);
 
-                printf("\n\t\tYOUR ZIP CODE      :");
-                scanf("%d",&S);
-                fprintf(vot1,"\t%d",S);
-
-                printf("\n\t\tYOUR DISTRICT      :");
-                scanf("%s",A);
-                fprintf(vot1,"\t%s",A);
-
                 printf("\n\t\tYOUR ADDRESS       :");
                 scanf("%s",Z);
                 fprintf(vot1,"\t%s",Z);
                 
                 printf("\n\t\tUSERNAME  :");
                 scanf("%S",X);
-                fprintf(vot2,"\t%s",X);
+                fprintf(candi2,"\n%s",X);
 
                 printf("\n\t\tPASSWORD  :");
                 scanf("%s",C);
-                fprintf(vot2,"\t%s",C);
+                fprintf(candi2,"\t%s",C);
 
                 printf("\t\t\t\t...........................................................\n\n");
 
@@ -373,9 +255,10 @@ int main(){
                 for(;!valid2;){
                     printf("\n\t\tENTER YOUR NATIONAL IDENTY CARD NUMBER _________");
                     printf("\n\t\tYou don't need to include the final characters of 'x' or 'v' in your national ID :");
+ 
                     scanf("%s",N);            
                     if(strlen(N)==12 ||strlen(N)==9){
-                        valid=2;
+                        valid2=1;
                     }else{
                         printf("\t\tIt's an invalid number!!! \n\t\tPlease enter a 9 or 12 Digit your national ID number.");
                         scanf("%s",N);
@@ -423,7 +306,6 @@ int main(){
             fclose(candi1);
             fclose(candi2);
             fclose(vot1);
-            fclose(vot2);
             fclose(party1);
             fclose(party2);
 
